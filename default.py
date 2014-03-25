f35399d402614ae986a02b208a3c310ea43aff2d
durdle <ddurdle@gmail.com>
2014-03-24 22:20:36 -0400

    initial

---
+++ default.py
@@ -0,0 +1,240 @@
+'''
+    cloudstream XBMC Plugin
+    Copyright (C) 2013 dmdsoftware
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from resources.lib import cloudstream
+import sys
+import urllib
+import cgi
+import re
+
+import xbmc, xbmcgui, xbmcplugin, xbmcaddon
+
+
+#helper methods
+def log(msg, err=False):
+    if err:
+        xbmc.log(addon.getAddonInfo('name') + ': ' + msg, xbmc.LOGERROR)
+    else:
+        xbmc.log(addon.getAddonInfo('name') + ': ' + msg, xbmc.LOGDEBUG)
+
+def parse_query(query):
+    queries = cgi.parse_qs(query)
+    q = {}
+    for key, value in queries.items():
+        q[key] = value[0]
+    q['mode'] = q.get('mode', 'main')
+    return q
+
+def addVideo(url, infolabels, label, img='', fanart='', total_items=0,
+                   cm=[], cm_replace=False):
+    infolabels = decode_dict(infolabels)
+    log('adding video: %s - %s' % (infolabels['title'], url))
+    listitem = xbmcgui.ListItem(label, iconImage=img,
+                                thumbnailImage=img)
+    listitem.setInfo('video', infolabels)
+    listitem.setProperty('IsPlayable', 'true')
+    listitem.setProperty('fanart_image', fanart)
+    if cm:
+        listitem.addContextMenuItems(cm, cm_replace)
+    xbmcplugin.addDirectoryItem(plugin_handle, url, listitem,
+                                isFolder=False, totalItems=total_items)
+
+def addDirectory(url, title, img='', fanart='', total_items=0):
+    log('adding dir: %s - %s' % (title, url))
+    listitem = xbmcgui.ListItem(decode(title), iconImage=img, thumbnailImage=img)
+    if not fanart:
+        fanart = addon.getAddonInfo('path') + '/fanart.jpg'
+    listitem.setProperty('fanart_image', fanart)
+    xbmcplugin.addDirectoryItem(plugin_handle, url, listitem,
+                                isFolder=True, totalItems=total_items)
+
+#http://stackoverflow.com/questions/1208916/decoding-html-entities-with-python/1208931#1208931
+def _callback(matches):
+    id = matches.group(1)
+    try:
+        return unichr(int(id))
+    except:
+        return id
+
+def decode(data):
+    return re.sub("&#(\d+)(;|(?=\s))", _callback, data).strip()
+
+def decode_dict(data):
+    for k, v in data.items():
+        if type(v) is str or type(v) is unicode:
+            data[k] = decode(v)
+    return data
+
+
+
+#global variables
+plugin_url = sys.argv[0]
+plugin_handle = int(sys.argv[1])
+plugin_queries = parse_query(sys.argv[2][1:])
+
+addon = xbmcaddon.Addon(id='plugin.video.cloudstream')
+
+try:
+
+    remote_debugger = addon.getSetting('remote_debugger')
+    remote_debugger_host = addon.getSetting('remote_debugger_host')
+
+    # append pydev remote debugger
+    if remote_debugger == 'true':
+        # Make pydev debugger works for auto reload.
+        # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
+        import pysrc.pydevd as pydevd
+        # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+        pydevd.settrace(remote_debugger_host, stdoutToServer=True, stderrToServer=True)
+except ImportError:
+    log(addon.getLocalizedString(30016), True)
+    sys.exit(1)
+except :
+    pass
+
+
+# retrieve settings
+username = addon.getSetting('username')
+password = addon.getSetting('password')
+auth_token = addon.getSetting('auth_token')
+auth_cookie = addon.getSetting('auth_cookie')
+user_agent = addon.getSetting('user_agent')
+save_auth_token = addon.getSetting('save_auth_token')
+
+
+# you need to have at least a username&password set or an authorization token
+#if ((username == '' or password == '') and auth_token == ''):
+#    xbmcgui.Dialog().ok(addon.getLocalizedString(30000), addon.getLocalizedString(30015))
+#    log(addon.getLocalizedString(30015), True)
+#    xbmcplugin.endOfDirectory(plugin_handle)
+
+
+#let's log in
+#cloudstream = cloudstream.cloudstream(username, password, auth_token, auth_cookie, user_agent)
+
+# if we don't have an authorization token set for the plugin, set it with the recent login.
+#   auth_token will permit "quicker" login in future executions by reusing the existing login session (less HTTPS calls = quicker video transitions between clips)
+#if auth_token == '' and save_auth_token == 'true':
+#    addon.setSetting('auth_token', cloudstream.auth)
+#    addon.setSetting('auth_cookie', cloudstream.cookie)
+
+
+
+log('plugin url: ' + plugin_url)
+log('plugin queries: ' + str(plugin_queries))
+log('plugin handle: ' + str(plugin_handle))
+
+mode = plugin_queries['mode']
+
+#dump a list of videos available to play
+if mode == 'main' or mode == 'folder':
+    log(mode)
+
+    movpod = movpod.movpod(username, password, auth_token, auth_cookie, user_agent)
+    folderID=0
+    if (mode == 'folder'):
+        folderID = plugin_queries['folderID']
+
+
+
+    cacheType = addon.getSetting('playback_type')
+
+#    videos = cloudstream.getVideosList()
+    videoURL = movpod.getPublicLink('http://movpod.in/89qq916c069c')
+    addVideo(videoURL,
+                             { 'title' : url , 'plot' : title }, url,
+                             img=None)
+
+#    folders = cloudstream.getFolderList(folderID)
+#    for title in sorted(folders.iterkeys()):
+#      addDirectory(folders[title],title)
+
+#    videos = cloudstream.getVideosList(folderID)
+#    for title in sorted(videos.iterkeys()):
+#      addVideo(videos[title]['url'],
+#                             { 'title' : title , 'plot' : title }, title,
+#                             img=videos[title]['thumbnail'])
+
+#play a URL that is passed in (presumely requires authorizated session)
+elif mode == 'play':
+    url = plugin_queries['url']
+
+    item = xbmcgui.ListItem(path=url)
+    log('play url: ' + url)
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+
+#play a video given its exact-title
+elif mode == 'playvideo' or mode == 'playVideo':
+    title = plugin_queries['title']
+    cacheType = addon.getSetting('playback_type')
+
+    if cacheType == '0':
+      videoURL = cloudstream.getVideoLink(title)
+    else:
+      videoURL = cloudstream.getVideoLink(title,True,cacheType)
+
+    item = xbmcgui.ListItem(path=videoURL)
+    log('play url: ' + videoURL)
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+
+
+
+#force stream - play a video given its exact-title
+elif mode == 'streamVideo' or mode == 'streamvideo':
+    try:
+      filename = plugin_queries['filename']
+    except:
+      title = 0
+
+
+    # immediately play resulting (is a video)
+    videoURL = cloudstream.getVideoLink(filename)
+    item = xbmcgui.ListItem(path=videoURL)
+    log('play url: ' + videoURL)
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+
+elif mode == 'streamURL' or mode == 'streamurl':
+    try:
+      url = plugin_queries['url']
+    except:
+      url = 0
+
+
+    # immediately play resulting (is a video)
+    videoURL = cloudstream.getPublicLink(url)
+    item = xbmcgui.ListItem(path=videoURL)
+    log('play url: ' + videoURL)
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+
+
+
+#clear the authorization token
+elif mode == 'clearauth':
+    addon.setSetting('auth_token', '')
+    addon.setSetting('auth_cookie', '')
+
+
+
+# update the authorization token in the configuration file if we had to login for a new one during this execution run
+if auth_token != cloudstream.auth and save_auth_token == 'true':
+    addon.setSetting('auth_token', cloudstream.auth)
+    addon.setSetting('auth_cookie', cloudstream.cookie)
+
+
+xbmcplugin.endOfDirectory(plugin_handle)
+
---
+++ resources/lib/movpod.py
@@ -0,0 +1,390 @@
+'''
+    movpod XBMC Plugin
+    Copyright (C) 2013 dmdsoftware
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+'''
+
+import os
+import re
+import urllib, urllib2
+import cookielib
+
+
+import xbmc, xbmcaddon, xbmcgui, xbmcplugin
+
+# global variables
+addon = xbmcaddon.Addon(id='plugin.video.movpod')
+
+# helper methods
+def log(msg, err=False):
+    if err:
+        xbmc.log(addon.getAddonInfo('name') + ': ' + msg, xbmc.LOGERROR)
+    else:
+        xbmc.log(addon.getAddonInfo('name') + ': ' + msg, xbmc.LOGDEBUG)
+
+
+#
+#
+#
+class movpod:
+
+
+    ##
+    # initialize (setting 1) username, 2) password, 3) authorization token, 4) user agent string
+    ##
+    def __init__(self, user, password, auth, user_agent):
+        self.user = user
+        self.password = password
+        self.auth = auth
+        self.user_agent = user_agent
+        self.cookiejar = cookielib.CookieJar()
+
+
+        # if we have an authorization token set, try to use it
+        if auth != '':
+          log('using token')
+
+          return
+        else:
+          log('no token - logging in')
+#          self.login();
+          return
+
+
+
+    ##
+    # perform login
+    ##
+    def login(self):
+
+        self.auth = ''
+
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookiejar))
+        # default User-Agent ('Python-urllib/2.6') will *not* work
+        opener.addheaders = [('User-Agent', self.user_agent)]
+
+        url = 'http://www.movpod.com/authenticate.php?login'
+
+        try:
+            response = opener.open(url)
+
+        except urllib2.URLError, e:
+            log(str(e), True)
+            return
+        response_data = response.read()
+        response.close()
+
+        # fetch captcha url
+        for r in re.finditer('<td>(CAPTCHA)</td>.*?<td><img src="([^\"]+)\"',
+                             response_data, re.DOTALL):
+            ceptchaType,captchaURL = r.groups()
+
+        url = 'http://www.movpod.com' + captchaURL
+
+        try:
+            response = opener.open(url)
+        except urllib2.URLError, e:
+                log(str(e), True)
+
+        output = open('/tmp/test.png','wb')
+        output.write(response.read())
+        output.close()
+        response.close()
+
+
+
+        img = xbmcgui.ControlImage(450,15,400,130,  '/tmp/test.png')
+        wdlg = xbmcgui.WindowDialog()
+        wdlg.addControl(img)
+        wdlg.show()
+
+        xbmc.sleep(3000)
+
+        kb = xbmc.Keyboard('', 'Type the letters in the image', False)
+        kb.doModal()
+        capcode = kb.getText()
+
+        if (kb.isConfirmed()):
+           userInput = kb.getText()
+           if userInput != '':
+               solution = kb.getText()
+           elif userInput == '':
+               raise Exception ('You must enter text in the image to access video')
+        else:
+           raise Exception ('Captcha Error')
+        wdlg.close()
+
+
+        url = 'http://www.movpod.com/authenticate.php?login'
+
+        values = {
+                  'pass' : self.password,
+                  'user' : self.user,
+                  'remember' : 1,
+                  'captcha_code' : solution,
+                  'login_submit' : 'Login',
+        }
+
+        log('logging in')
+
+        # try login
+        try:
+            response = opener.open(url,urllib.urlencode(values))
+
+        except urllib2.URLError, e:
+            if e.code == 403:
+                #login denied
+                xbmcgui.Dialog().ok(ADDON.getLocalizedString(30000), ADDON.getLocalizedString(30017))
+            log(str(e), True)
+            return
+        response_data = response.read()
+        response.close()
+
+
+        loginResult = 0
+        #validate successful login
+        for r in re.finditer('class="(header-right-auth)"><strong>([^\<]+)</strong>',
+                             response_data, re.DOTALL):
+            loginType,loginResult = r.groups()
+
+        if (loginResult == 0 or loginResult != self.user):
+            xbmcgui.Dialog().ok(ADDON.getLocalizedString(30000), ADDON.getLocalizedString(30017))
+            log('login failed', True)
+            return
+
+        for cookie in self.cookiejar:
+            for r in re.finditer(' ([^\=]+)\=([^\s]+)\s',
+                        str(cookie), re.DOTALL):
+                cookieType,cookieValue = r.groups()
+                if cookieType == 'auth':
+                    self.auth = cookieValue
+
+
+        return
+
+
+
+    ##
+    # return the appropriate "headers" for FireDrive requests that include 1) user agent, 2) authorization cookie
+    #   returns: list containing the header
+    ##
+    def getHeadersList(self):
+        if (self.cookie != '' or self.cookie != 0):
+            return { 'User-Agent' : self.user_agent, 'Cookie' : 'auth='+self.auth+'; exp=1' }
+        else:
+            return { 'User-Agent' : self.user_agent }
+
+    ##
+    # return the appropriate "headers" for FireDrive requests that include 1) user agent, 2) authorization cookie
+    #   returns: URL-encoded header string
+    ##
+    def getHeadersEncoded(self):
+        return urllib.urlencode(self.getHeadersList())
+
+    ##
+    # retrieve a list of videos, using playback type stream
+    #   parameters: prompt for video quality (optional), cache type (optional)
+    #   returns: list of videos
+    ##
+    def getVideosList(self, folderID=0, cacheType=0):
+
+        # retrieve all documents
+        url = 'http://www.movpod.com/cp.php'
+
+        videos = {}
+        if True:
+            log('url = %s header = %s' % (url, self.getHeadersList()))
+            req = urllib2.Request(url, None, self.getHeadersList())
+
+            # if action fails, validate login
+            try:
+              response = urllib2.urlopen(req)
+            except urllib2.URLError, e:
+              if e.code == 403 or e.code == 401:
+                self.login()
+                req = urllib2.Request(url, None, self.getHeadersList())
+                try:
+                  response = urllib2.urlopen(req)
+                except urllib2.URLError, e:
+                  log(str(e), True)
+                  return
+              else:
+                log(str(e), True)
+                return
+
+            response_data = response.read()
+            response.close()
+
+
+            # parsing page for videos
+            # video-entry
+            for r in re.finditer('input name="file_\d+" type="checkbox" value="([^\"]+)"></td>.*?<div style="background-image:url\(([^\)]+)\);.*?<strong><a href="[^\"]+">([^\<]+)</a>' ,
+                                 response_data, re.DOTALL):
+                fileID,img,title = r.groups()
+
+
+                log('found video %s %s' % (title, fileID))
+
+                # streaming
+                videos[title] = {'url': 'plugin://plugin.video.movpod?mode=streamVideo&filename=' + fileID, 'thumbnail' : img}
+
+
+
+        return videos
+
+
+    ##
+    # retrieve a list of folders
+    #   parameters: folder is the current folderID
+    #   returns: list of videos
+    ##
+    def getFolderList(self, folderID=0):
+
+        # retrieve all documents
+        params = urllib.urlencode({'getFolders': folderID, 'format': 'large', 'term': '', 'group':0, 'user_token': self.auth, '_': 1394486104901})
+
+        url = 'http://www.movpod.in/'+ params
+
+        folders = {}
+        if True:
+            log('url = %s header = %s' % (url, self.getHeadersList()))
+            req = urllib2.Request(url, None, self.getHeadersList())
+
+            # if action fails, validate login
+            try:
+              response = urllib2.urlopen(req)
+            except urllib2.URLError, e:
+              if e.code == 403 or e.code == 401:
+                self.login()
+                req = urllib2.Request(url, None, self.getHeadersList())
+                try:
+                  response = urllib2.urlopen(req)
+                except urllib2.URLError, e:
+                  log(str(e), True)
+                  return
+              else:
+                log(str(e), True)
+                return
+
+            response_data = response.read()
+
+            # parsing page for videos
+            # video-entry
+            for r in re.finditer('"f_id":"([^\"]+)".*?"f_fullname":"([^\"]+)"' ,response_data, re.DOTALL):
+                folderID, folderName = r.groups()
+
+                log('found folder %s %s' % (folderID, folderName))
+
+                # streaming
+                folders[folderName] = 'plugin://plugin.video.movpod?mode=folder&folderID=' + folderID
+
+            response.close()
+
+        return folders
+
+
+
+    ##
+    # retrieve a video link
+    #   parameters: title of video, whether to prompt for quality/format (optional), cache type (optional)
+    #   returns: list of URLs for the video or single URL of video (if not prompting for quality)
+    ##
+    def getVideoLink(self,filename,cacheType=0):
+
+
+
+        return 'http://dl.movpod.com/?alias='+filename+'&stream' + '|'+self.getHeadersEncoded()
+
+    ##
+    # retrieve a video link
+    #   parameters: title of video, whether to prompt for quality/format (optional), cache type (optional)
+    #   returns: list of URLs for the video or single URL of video (if not prompting for quality)
+    ##
+    def getPublicLink(self,url,cacheType=0):
+
+
+        log('url = %s header = %s' % (url, self.getHeadersList()))
+        req = urllib2.Request(url, None, self.getHeadersList())
+
+
+        # if action fails, validate login
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.URLError, e:
+            if e.code == 403 or e.code == 401:
+              self.login()
+              req = urllib2.Request(url, None, self.getHeadersList())
+              try:
+                response = urllib2.urlopen(req)
+              except urllib2.URLError, e:
+                log(str(e), True)
+                return
+            else:
+              log(str(e), True)
+              return
+
+        response_data = response.read()
+        response.close()
+
+
+        confirmID = 0
+        # fetch video title, download URL and docid for stream link
+        for r in re.finditer('<input type="hidden" name="op" value="([^\"]+)">.*?<input type="hidden" name="usr_login" value="([^\"]*)">.*?<input type="hidden" name="id" value="([^\"]+)">.*?<input type="hidden" name="fname" value="([^\"]*)">.*?<input type="hidden" name="referer" value="([^\"]*)">' ,response_data, re.DOTALL):
+             op,usr_login,id,fname,referer = r.groups()
+
+
+        values = {
+                  'op' : op,
+                  'usr_login' : usr_login,
+                  'id' : id,
+                  'fname' : fname,
+                  'referer' : referer
+        }
+
+        req = urllib2.Request(url, urllib.urlencode(values), self.getHeadersList())
+
+
+        # if action fails, validate login
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.URLError, e:
+            if e.code == 403 or e.code == 401:
+              self.login()
+              req = urllib2.Request(url,  urllib.urlencode(values), self.getHeadersList())
+              try:
+                response = urllib2.urlopen(req)
+              except urllib2.URLError, e:
+                log(str(e), True)
+                return
+            else:
+              log(str(e), True)
+              return
+
+        response_data = response.read()
+        response.close()
+
+        streamURL = 0
+        # fetch video title, download URL and docid for stream link
+        for r in re.finditer('(file)\: \"([^\"]+)"\,' ,response_data, re.DOTALL):
+             streamType,streamURL = r.groups()
+
+
+        return streamURL
+
+
+
+
